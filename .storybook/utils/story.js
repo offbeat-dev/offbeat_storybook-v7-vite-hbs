0886e66c86f847763d1f316fe689301ba50f5412
offbeat-dev <offbeat.it.all@gmail.com>
2022-12-30 12:58:21 -0500

    :construction: -- to do: figure whye actions disable makes react crash

--- .storybook/utils/story.js
+++ .storybook/utils/story.js
@@ -2,6 +2,24 @@ const story = {
   build: (template, args, layout = "fullscreen") => {
     const Template = ((args) => template({ ...args })).bind({});
     Template.args = args;
+
+    Template.parameters = {
+      layout,
+      docs: {
+        source: {
+          code: template({ ...args })
+            .replace(/\uFEFF/gi, "")
+            .replace(/^\s*\n/gm, "")
+            .replace(/&#x3D;/g, "=")
+            .replace(/&amp;/g, "&")
+            .replace(/&lt;/g, "<")
+            .replace(/&gt;/g, ">")
+            .replace(/&quot;/g, '"')
+            .replace(/&#039;/g, "'"),
+        },
+      },
+    };
+
     return Template;
   },
 };
